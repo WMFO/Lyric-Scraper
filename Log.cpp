e868ebfa32aed40b87c8d434cbf83d58f2a6ddc9
Joshua Liebow-Feeser <joshlf13@gmail.com>
2012-10-06 15:12:38 -0400

    Added logging interface and class

--- Log.cpp
+++ Log.cpp
@@ -1,9 +1,45 @@
-//
-//  Log.cpp
-//  Lyric-Scraper
-//
-//  Created by  on 10/6/12.
-//  Copyright 2012 __MyCompanyName__. All rights reserved.
-//
-
+#include "Log.h"
 #include <iostream>
+using namespace std;
+
+Log::Log() {
+    normal = NULL;
+    error  = NULL;
+    fatal  = NULL;
+}
+
+Log::Log(logger n, logger e, logger f) {
+    normal = n;
+    error  = e;
+    fatal  = f;
+}
+
+int Log::setFunc(logger l, int level) {
+    switch (level) {
+        case NORMAL:
+            normal = l;
+            return 1;
+        case ERROR:
+            error = l;
+            return 1;
+        case FATAL:
+            fatal = l;
+            return 1;
+    }
+    return 0;
+}
+
+int Log::log(string msg, int level) {
+    switch (level) {
+        case NORMAL:
+            normal(msg);
+            return 1;
+        case ERROR:
+            error(msg);
+            return 1;
+        case FATAL:
+            fatal(msg);
+            return 1;
+    }
+    return 0;
+}
--- Log.h
+++ Log.h
@@ -1,14 +1,33 @@
-//
-//  Log.h
-//  Lyric-Scraper
-//
-//  Created by  on 10/6/12.
-//  Copyright 2012 __MyCompanyName__. All rights reserved.
-//
+#include <iostream>
+
+// Log levels
+#define NORMAL 0
+#define ERROR 1
+#define FATAL 2
+
+using namespace std;
+
+// Function signature for logger
+typedef void(*logger)(string err);
 
 #ifndef Lyric_Scraper_Log_h
 #define Lyric_Scraper_Log_h
 
-
+class Log {
+public:
+    Log();
+    // Initialize with functions to be called on the various logging levels
+    Log(logger normal, logger error, logger fatal);
+    
+    // Set the function to be called on logging level, l
+    // Return 1 upon success, else 0
+    int setFunc(logger l, int level);
+    
+    // Log an error
+    // Return 1 upon success, else 0
+    int log(string err, int level);
+private:
+    logger normal, error, fatal;
+};
 
 #endif
