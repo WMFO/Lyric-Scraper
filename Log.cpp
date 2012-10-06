313a415e23c10d37b79ff2d62bd24ac8d7462d1b
Joshua Liebow-Feeser <joshlf13@gmail.com>
2012-10-06 14:49:33 -0400

    Added logging files

---
+++ Log.cpp
@@ -0,0 +1,9 @@
+//
+//  Log.cpp
+//  Lyric-Scraper
+//
+//  Created by  on 10/6/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ Log.h
@@ -0,0 +1,14 @@
+//
+//  Log.h
+//  Lyric-Scraper
+//
+//  Created by  on 10/6/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Lyric_Scraper_Log_h
+#define Lyric_Scraper_Log_h
+
+
+
+#endif
