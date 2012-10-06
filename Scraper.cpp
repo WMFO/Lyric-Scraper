89173f32cf00fb6d68ae1b07102f1c04bed239f4
Joshua Liebow-Feeser <joshlf13@gmail.com>
2012-10-06 16:52:45 -0400

    Turned lyrics into a class higherarchy

--- Scraper.cpp
+++ Scraper.cpp
@@ -12,6 +12,7 @@
 #include <string>
 #include <assert.h>
 #include <fstream>
+#include <vector>
 #include "Lyrics.h"
 #include "Log.h"
 
@@ -20,6 +21,8 @@
 
 // Error codes
 #define USAGE 1
+#define NOTFOUND 2
+
 
 using namespace std;
 
@@ -55,19 +58,30 @@ int main (int argc, char *argv[]) {
         logger.log("Song: " + song, LOG_NORMAL);
         logger.log("Band: " + band, LOG_NORMAL);
     }
-
-    int nsites = numSites();
-    for (int i = 0; i < nsites; i++){
-        string result = lyrics(song, band, i);
-        if (result.length() > MIN_LYR_LEN){
-            if (OUTPUT) cout << result << endl;
-            break;
+    
+    Lyrics l = Lyrics();
+    vector<string> lyrics = l.lyrics(song, band);
+    for (int i = 0; i < lyrics.size(); i++) {
+        if (lyrics[i].length() > MIN_LYR_LEN) {
+            logger.log(lyrics[i], LOG_NORMAL);
         }
     }
+
+//    int nsites = numSites();
+//    for (int i = 0; i < nsites; i++){
+//        string result = lyrics(song, band, i);
+//        if (result.length() > MIN_LYR_LEN){
+//            if (OUTPUT) cout << result << endl;
+//            break;
+//        }
+//    }
     
     return 0;
 }
 
-void log_normal(string err) { cout    << err << endl; }
+void log_normal(string err) { 
+    if (OUTPUT)
+        cout << err << endl; 
+}
 void log_error (string err) { logfile << err << endl; }
 void log_fatal (string err) { logfile << err << endl; }
