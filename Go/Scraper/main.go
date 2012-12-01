5df4a122a4aa31b61c23ab2a90781d911002c4e5
Connor Blanck <connorblanck@gmail.com>
2012-12-01 15:45:32 -0500

    Working on making a proper Regexp type for each expression so we can have better logging (i.e. saying which song/which word was dirty)

--- Go/Scraper/main.go
+++ Go/Scraper/main.go
@@ -15,56 +15,56 @@ func main() {
 
 	loadPatterns(file)
 
-	net, err := os.OpenFile("networkErrors.log", os.O_WRONLY | os.O_APPEND, 0660)
+	net, err := os.OpenFile("networkErrors.log", os.O_WRONLY|os.O_APPEND, 0660)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: could not open %s: %s\n", "networkErrors.log", err.Error());
+		fmt.Fprintf(os.Stderr, "Error: could not open %s: %s\n", "networkErrors.log", err.Error())
 		os.Exit(1)
 	}
-	song, err := os.OpenFile("songClassification.log", os.O_WRONLY | os.O_APPEND, 0660)
+	song, err := os.OpenFile("songClassification.log", os.O_WRONLY|os.O_APPEND, 0660)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: could not open %s: %s\n", "songClassification.log", err.Error());
+		fmt.Fprintf(os.Stderr, "Error: could not open %s: %s\n", "songClassification.log", err.Error())
 		os.Exit(1)
 	}
-	dirty, err := os.OpenFile("dirtySongs.log", os.O_WRONLY | os.O_APPEND, 0660)
+	dirty, err := os.OpenFile("dirtySongs.log", os.O_WRONLY|os.O_APPEND, 0660)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: could not open %s: %s\n", "dirtySongs.log", err.Error());
+		fmt.Fprintf(os.Stderr, "Error: could not open %s: %s\n", "dirtySongs.log", err.Error())
 		os.Exit(1)
 	}
-	regex, err := os.OpenFile("regex.log", os.O_WRONLY | os.O_APPEND, 0660)
+	regex, err := os.OpenFile("regex.log", os.O_WRONLY|os.O_APPEND, 0660)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error: could not open %s: %s\n", "regex.log", err.Error());
+		fmt.Fprintf(os.Stderr, "Error: could not open %s: %s\n", "regex.log", err.Error())
 		os.Exit(1)
 	}
 
 	initLogging(net, song, dirty, regex)
 
 	err = connect("root", "password", "Rivendell")
-	
+
 	if err != nil {
 		networkErrors.Printf("SQL ERROR: %s", err.Error())
 		os.Exit(2)
 	}
-	
+
 	checkNSongs(20)
 }
 
 func checkNSongs(n int) {
-	
+
 	// Get n songs
 	rows := search(n)
-	
+
 	// If the search failed
 	// Don't log the error here - it will have been logged in the mysql module
 	if rows == nil {
 		return
 	}
-	
+
 	for rows.Next() {
 		var number int
 		var artist, title, album string
 		err := rows.Scan(&number, &artist, &title, &album)
 		dirty, err := checkAllSites(title, artist)
-		
+
 		var code string
 		var log string
 		if err != nil {
@@ -79,8 +79,8 @@ func checkNSongs(n int) {
 			log = "CLEAN"
 		}
 		q := fmt.Sprintf("UPDATE CART SET SCHED_CODES='%s' WHERE NUMBER='%d'", code, number)
-        fmt.Printf("%s\n", q);
-        err = query(q)
+		fmt.Printf("%s\n", q)
+		err = query(q)
 		if err != nil {
 			networkErrors.Printf("SQL ERROR: %s", err.Error())
 		}
--- Go/Scraper/regex.go
+++ Go/Scraper/regex.go
@@ -5,30 +5,39 @@ import (
 	"io"
 	"regexp"
 	"strings"
-    "fmt"
 )
 
 var patterns []string
+var regexps *[]regexp.Regexp
 
 // Checks against each regex in the patterns array
 // and returns true if any of them matched
 func dirty(lyrics string) bool {
-	for _, p := range patterns {
-        if(p == ""){
-            continue
-        }
-		match, err := regexp.MatchString(p, lyrics)
-		if err != nil {
-			regexErrors.Printf("REGEX: %s\nLYRICS: %s", p, lyrics)
-		} else if match {
-            fmt.Printf("Found match %s\n", p)
+	for _, r := range regexps {
+		match := r.FindString(lyrics)
+		if match != "" {
+			dirtySong.Printf("Bad word: %s matches regexp: %s in song: %s by artist: %s", match, r.String(), "N/A", "N/A")
 			return true
 		}
 	}
 	return false
+
+	/*for _, p := range patterns {
+	        if(p == ""){
+	            continue
+	        }
+			match, err := regexp.MatchString(p, lyrics)
+			if err != nil {
+				regexErrors.Printf("REGEX: %s\nLYRICS: %s", p, lyrics)
+			} else if match {
+	            fmt.Printf("Found match %s\n", p)
+				return true
+			}
+		}
+		return false*/
 }
 
-// Load patterns line by line into the patterns slice
+// Create proper regex type
 func loadPatterns(r io.Reader) error {
 	buf := new(bytes.Buffer)
 	_, err := buf.ReadFrom(r)
@@ -38,5 +47,25 @@ func loadPatterns(r io.Reader) error {
 	}
 
 	patterns = strings.Split(buf.String(), "\n")
+
+	for i, p := range patterns {
+		if p == "" {
+			continue
+		}
+		regexps[i], _ = Compile(p)
+	}
 	return nil
 }
+
+// Load patterns line by line into the patterns slice
+/*func loadPatterns(r io.Reader) error {
+	buf := new(bytes.Buffer)
+	_, err := buf.ReadFrom(r)
+
+	if err != nil {
+		return err
+	}
+
+	patterns = strings.Split(buf.String(), "\n")
+	return nil
+}*/
